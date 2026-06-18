8fb550de546e34a1db55f5152201042c2445dac4
Kaan Tekin Öztekin <kaan.oztkn99@gmail.com>
2026-06-18 14:30:00 +0200

    add scene manager script

---
+++ Assets/Scripts/GameSceneManager.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameSceneManager : MonoBehaviour
+{
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void RestartCurrentScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
